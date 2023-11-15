77e826f685300b6b8823a39e13f384ff949e33ca
Nate Obray <nathanobray@gmail.com>
2023-11-15 00:19:36 +0000

    added google analytics

--- src/controllers/Index.php
+++ src/controllers/Index.php
@@ -20,16 +20,24 @@ class Index
     public function get()
     {
         $this->html = '<html>
-        <head>
-            <title>ZAF - Personal Injury</title>
-            <link rel="stylesheet" href="/assets/css/style.css">
-            <link rel="stylesheet" href="/assets/css/all.min.css">
-            <script type="module" src="/assets/js/App.js"></script>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        </head>
-        <body>
+            <head>
+                <title>ZAF - Personal Injury</title>
+                <link rel="stylesheet" href="/assets/css/style.css">
+                <link rel="stylesheet" href="/assets/css/all.min.css">
+                <script type="module" src="/assets/js/App.js"></script>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-4JL8CCKYB2"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag(\'js\', new Date());
             
-        </body>
-    </html>';
+            gtag(\'config\', \'G-4JL8CCKYB2\');
+            </script>
+            </body>
+        </html>';
     }
 }
