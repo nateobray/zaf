ca9c2bd17bea5d298e5276096175f9f61c09e16a
Nate Obray <nathanobray@gmail.com>
2024-03-15 18:02:57 +0000

    remove duplicate gtag stuff

--- src/controllers/Index.php
+++ src/controllers/Index.php
@@ -29,16 +29,6 @@ class Index
             </head>
             <body>
             
-                <!-- Google tag (gtag.js) -->
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-4JL8CCKYB2"></script>
-                <script>
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag(\'js\', new Date());
-
-                    gtag(\'config\', \'G-4JL8CCKYB2\');
-                    gtag(\'config\', \'AW-11482071246\');
-                </script>
             </body>
         </html>';
     }
